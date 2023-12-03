5c21efe6847e88082d679f616ef74a324740142e
MarioFreireArias <202105835@alu.comillas.edu>
2023-12-03 18:19:38 +0100

    Restored previous version of the program

--- robot_controller.py
+++ robot_controller.py
@@ -5,7 +5,7 @@ from gpiozero.pins.pigpio import PiGPIOFactory
 from simple_pid import PID
 from tracker import Tracker
 import RobotControl.remove_servo_jitter as remove_jitter
-# import RobotControl.led_controller as led_controller
+import RobotControl.led_controller as led_controller
 import time
 import math
 
@@ -97,53 +97,6 @@ class Robot_Controller:
 #obj_detected = False
 
 if __name__ == '__main__':
-<<<<<<< HEAD
-    try:
-        servo_x= Servo(PIN_X, pin_factory=PinFactory, min_pulse_width=0.0005, max_pulse_width=0.0025)
-        servo_y = Servo(PIN_Y, pin_factory=PinFactory, min_pulse_width=0.0005, max_pulse_width=0.0025)
-        servo_x.value = 0
-        servo_y.value = 0
-        PID_x = PID(0.1, 1, 0.005, output_limits=(-1, 1), setpoint=0, starting_output=0)
-        PID_y = PID(0.075, 0.75, 0.005, output_limits=(-0.5, 0.5), setpoint=0, starting_output=0)
-        global start_control
-        start_control = False
-        def move_robot(center):
-            #Moves the robot to point at the center of the object
-            # center is a tuple (x, y)
-            
-            if center is None:
-                servo_x.value = 0
-                servo_y.value = 0
-               # led_controller.change_effect("breathe")
-                return
-            
-            error_x = (center[0] - 320) / 320
-            error_y = (center[1] - 240) / 240
-            angle_x = PID_x(error_x)
-            angle_y = PID_y(error_y)
-            
-            global start_control
-            if not start_control:
-                if angle_x != -1 and angle_x != 1 and angle_y != -1 and angle_y != 1:
-                    start_control = True
-                return
-            
-        # Invert angle
-            if INVERT_X:
-                angle_x = -angle_x
-            if INVERT_Y:
-                angle_y = -angle_y
-
-            servo_x.value = angle_x
-            servo_y.value = angle_y
-           # led_controller.change_effect("static_color")
-
-    #Set up LEDs
-       # led_controller.change_brightness(BRIGHTNESS)
-       # led_controller.change_color(COLOR)
-       # led_controller.change_effect("static_color")
-        tracker = Tracker(on_new_frame_function=move_robot, color=COLOR)
-=======
 
     #Remove jitter from servo 
     remove_jitter.remove_jitter()
@@ -179,26 +132,30 @@ if __name__ == '__main__':
     def move_robot(center):
         #Moves the robot to point at the center of the object
         # center is a tuple (x, y)
->>>>>>> 89b061459382f29561ac99bf403fe86c727facbf
         
-        #Closing sequence 
-        def close_program():
-            print("closing all systems")
-            tracker.close()
-
+        if center is None:
             servo_x.value = 0
             servo_y.value = 0
+            led_controller.change_effect("breathe")
+            return
+        
+        error_x = (center[0] - 320) / 320
+        error_y = (center[1] - 240) / 240
+        angle_x = PID_x(error_x)
+        angle_y = PID_y(error_y)
+        
+        global start_control
+        if not start_control:
+            if angle_x != -1 and angle_x != 1 and angle_y != -1 and angle_y != 1:
+                start_control = True
+            return
+        
+       # Invert angle
+        if INVERT_X:
+            angle_x = -angle_x
+        if INVERT_Y:
+            angle_y = -angle_y
 
-<<<<<<< HEAD
-            servo_x.close()
-            servo_y.close()
-           # led_controller.stop_all()
-           # GPIO.cleanup()
-            exit()
-
-    #Wait for key when ready
-        input("Press enter to start tracking")
-=======
         servo_x.value = angle_x
         servo_y.value = angle_y
         led_controller.change_effect("static_color")"""
@@ -211,25 +168,16 @@ if __name__ == '__main__':
     
    #Wait for key when ready
     input("Press enter to start tracking")
->>>>>>> 89b061459382f29561ac99bf403fe86c727facbf
 
-        tracker.track()
-        
-        close_program()
+    tracker.track()
+    
+   #Closing sequence 
+    tracker.close()
 
-    except KeyboardInterrupt:
-        close_program()
-        
+    servo_x.value = 0
+    servo_y.value = 0
 
-<<<<<<< HEAD
-    except Exception as e:
-        print(e)
-        close_program()
-        
-
-=======
     servo_x.close()
     servo_y.close()
 """
->>>>>>> 89b061459382f29561ac99bf403fe86c727facbf
     
